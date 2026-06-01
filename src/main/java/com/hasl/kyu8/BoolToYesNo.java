11a1a8c25a84f8bb97f7b2e038bc6b29e700cd5f
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-01 20:37:05 +0500

    BoolToYesNo_two

--- src/main/java/com/hasl/kyu8/BoolToYesNo.java
+++ src/main/java/com/hasl/kyu8/BoolToYesNo.java
@@ -2,7 +2,7 @@ package com.hasl.kyu8;
 
 public class BoolToYesNo {
     public static String boolToWord(boolean b) {
-        return b == true ? "Yes" : "No";
+        return b ? "Yes" : "No";
     }
 }
 
