3e200e5f0e37a310442c92258d8aa899d103cdb1
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-01 21:12:52 +0500

    sumOfPositive

--- src/main/java/com/hasl/kyu8/SumOfPositive.java
+++ src/main/java/com/hasl/kyu8/SumOfPositive.java
@@ -2,7 +2,18 @@ package com.hasl.kyu8;
 
 public class SumOfPositive {
     public static int sumOfPositive(int[] arr) {
-        // TODO: вернуть сумму всех положительных чисел. Если массив пустой или null — вернуть 0
-        return 0;
+        if (arr == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                sum = sum + arr[i];
+            }
+        }
+        return sum;
     }
 }
+
+// TODO: вернуть сумму всех положительных чисел. Если массив пустой или null — вернуть 0
