23ffd67af7e165ca60867b733e628d7d06f89f65
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-01 17:54:25 +0500

    refactor: EvenOrOdd (kyu8) with ternary

--- src/main/java/com/hasl/kyu8/EvenOrOdd.java
+++ src/main/java/com/hasl/kyu8/EvenOrOdd.java
@@ -2,11 +2,6 @@ package com.hasl.kyu8;
 
 public class EvenOrOdd {
     public static String evenOrOdd(int number) {
-        String result = "";
-
-        if (number % 2 == 0) {
-            result = "Even";
-        } else result = "Odd";
-        return result;
+        return number % 2 == 0 ? "Even" : "Odd";
     }
 }
