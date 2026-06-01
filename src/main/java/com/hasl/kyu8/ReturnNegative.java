ed103dcc8d235a1c76116703ec92f1baf787bc0f
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-01 18:15:18 +0500

    fix: ReturnNegative proper ternary

--- src/main/java/com/hasl/kyu8/ReturnNegative.java
+++ src/main/java/com/hasl/kyu8/ReturnNegative.java
@@ -2,19 +2,8 @@ package com.hasl.kyu8;
 
 public class ReturnNegative {
     public static int makeNegative(int x) {
-        int result = 0;
-
-        if (x < 0) {
-            result = x * 1;
-        }
-
-        if (x == 0) {
-            result = 0;
-        }
-        if (x > 0) {
-            result = x * (-1);
-        }
-        return result;
+        return x > 0 ? -x : x;
     }
 }
+
 // TODO: вернуть отрицательное число. Если x уже отрицательный или 0 — вернуть как есть
