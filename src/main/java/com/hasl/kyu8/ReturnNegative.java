b1059843d8e45755daf63a70442c7eb4fa961753
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-01 18:09:24 +0500

    ReturnNegative_one

--- src/main/java/com/hasl/kyu8/ReturnNegative.java
+++ src/main/java/com/hasl/kyu8/ReturnNegative.java
@@ -2,7 +2,19 @@ package com.hasl.kyu8;
 
 public class ReturnNegative {
     public static int makeNegative(int x) {
-        // TODO: вернуть отрицательное число. Если x уже отрицательный или 0 — вернуть как есть
-        return 0;
+        int result = 0;
+
+        if (x < 0) {
+            result = x * 1;
+        }
+
+        if (x == 0) {
+            result = 0;
+        }
+        if (x > 0) {
+            result = x * (-1);
+        }
+        return result;
     }
 }
+// TODO: вернуть отрицательное число. Если x уже отрицательный или 0 — вернуть как есть
