f58a43441b4785ba0148c0cd4da2603f7d5ea83c
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-02 15:50:14 +0500

    MapDouble

--- src/main/java/com/hasl/kyu8/MapDouble.java
+++ src/main/java/com/hasl/kyu8/MapDouble.java
@@ -2,7 +2,16 @@ package com.hasl.kyu8;
 
 public class MapDouble {
     public static int[] map(int[] arr) {
-        // TODO: вернуть массив, где каждый элемент умножен на 2
-        return new int[]{};
+        if (arr == null || arr.length == 0) {
+            return new int[]{};
+        }
+
+        int[] arrBuffer = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arrBuffer[i] = arr[i] * 2;
+        }
+        return arrBuffer;
     }
 }
+
+// TODO: вернуть массив, где каждый элемент умножен на 2
