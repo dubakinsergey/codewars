a9d0f8a6ba207a99669c506a3133cbd100c1703f
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-02 13:25:02 +0500

    solve: SquareSum (kyu8)

--- src/main/java/com/hasl/kyu8/SquareSum.java
+++ src/main/java/com/hasl/kyu8/SquareSum.java
@@ -2,7 +2,13 @@ package com.hasl.kyu8;
 
 public class SquareSum {
     public static int squareSum(int[] numbers) {
-        // TODO: вернуть сумму квадратов всех элементов массива
-        return 0;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + (numbers[i] * numbers[i]);
+        }
+        return sum;
     }
 }
+
+// TODO: вернуть сумму квадратов всех элементов массива
