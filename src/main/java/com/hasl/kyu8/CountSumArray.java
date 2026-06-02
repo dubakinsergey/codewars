82708b7fd3117fbc934c538a8a509561d5f4424c
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-02 14:21:14 +0500

    solve: CountSumArray (kyu8)

--- src/main/java/com/hasl/kyu8/CountSumArray.java
+++ src/main/java/com/hasl/kyu8/CountSumArray.java
@@ -2,8 +2,25 @@ package com.hasl.kyu8;
 
 public class CountSumArray {
     public static int[] countPositivesSumNegatives(int[] input) {
-        // TODO: вернуть массив [количество положительных, сумма отрицательных]
-        // Если input null или пустой — вернуть пустой массив
-        return new int[]{};
+
+        if (input == null || input.length == 0) {
+            return new int[]{};
+        }
+
+        int numberPositive = 0;
+        int sumNegative = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] < 0) {
+                sumNegative = sumNegative + input[i];
+            }
+            if (input[i] > 0) {
+                numberPositive = numberPositive + 1;
+            }
+        }
+        return new int[]{numberPositive, sumNegative};
     }
 }
+
+// TODO: вернуть массив [количество положительных, сумма отрицательных]
+// Если input null или пустой — вернуть пустой массив
