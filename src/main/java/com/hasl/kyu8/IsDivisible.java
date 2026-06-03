16b4be490df1e84d58e8dd2c1a39b95417d662da
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-03 13:28:38 +0500

    solve: IsDivisible (kyu8)

--- src/main/java/com/hasl/kyu8/IsDivisible.java
+++ src/main/java/com/hasl/kyu8/IsDivisible.java
@@ -2,7 +2,8 @@ package com.hasl.kyu8;
 
 public class IsDivisible {
     public static boolean isDivisible(int n, int x, int y) {
-        // TODO: вернуть true если n делится на x И на y без остатка
-        return false;
+        return n % x == 0 && n % y == 0;
     }
 }
+
+// TODO: вернуть true если n делится на x И на y без остатка
--- src/test/java/com/hasl/kyu8/IsDivisibleTest.java
+++ src/test/java/com/hasl/kyu8/IsDivisibleTest.java
@@ -1,6 +1,7 @@
 package com.hasl.kyu8;
 
 import org.testng.annotations.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class IsDivisibleTest {
