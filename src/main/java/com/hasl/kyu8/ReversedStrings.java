e520f071f66a74ea01f82b781033930b06183883
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-03 10:38:42 +0500

    feat: add both reverse implementations with tests

--- src/main/java/com/hasl/kyu8/ReversedStrings.java
+++ src/main/java/com/hasl/kyu8/ReversedStrings.java
@@ -1,8 +1,18 @@
 package com.hasl.kyu8;
 
 public class ReversedStrings {
-    public static String reverse(String str) {
+    public static String reverseStringBuilder(String str) {
         StringBuilder stringBuilder = new StringBuilder(str);
         return stringBuilder.reverse().toString();
+    }
+
+    public static String reverse(String str) {
+        char[] arrChars = str.toCharArray();
+        String result = "";
+
+        for (int i = arrChars.length - 1; i >= 0; i--) {
+            result = result + arrChars[i];
+        }
+        return result;
     }
 }
