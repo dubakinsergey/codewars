f644fe6dab234e31ac94ecbf614308deb9bdb3e7
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-04 13:26:57 +0500

    CenturyFromYear

--- src/main/java/com/hasl/kyu8/CenturyFromYear.java
+++ src/main/java/com/hasl/kyu8/CenturyFromYear.java
@@ -2,7 +2,8 @@ package com.hasl.kyu8;
 
 public class CenturyFromYear {
     public static int century(int year) {
-        // TODO: вернуть век (1900 = 19 век, 1901 = 20 век)
-        return 0;
+        return (year + 99) / 100;
     }
 }
+
+// TODO: вернуть век (1900 = 19 век, 1901 = 20 век)
