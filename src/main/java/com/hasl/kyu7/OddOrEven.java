f009f4c3d5192b8997a1bb1296ce995ae3bf540a
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-07 12:03:26 +0500

    solve: OddOrEven (kyu7)

--- src/main/java/com/hasl/kyu7/OddOrEven.java
+++ src/main/java/com/hasl/kyu7/OddOrEven.java
@@ -2,9 +2,15 @@ package com.hasl.kyu7;
 
 public class OddOrEven {
     public static String oddOrEven(int[] array) {
-        // TODO: [0] → "even"
-        // [1, 2, 3] → "even" (1+2+3=6)
-        // [1, 2, 4] → "odd" (1+2+4=7)
-        return "";
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return (sum % 2 == 0) ? "even" : "odd";
     }
 }
+
+// TODO: [0] → "even"
+// [1, 2, 3] → "even" (1+2+3=6)
+// [1, 2, 4] → "odd" (1+2+4=7)
