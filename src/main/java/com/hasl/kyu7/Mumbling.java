f0f677f0ecd84cf7c8004ab03c103455ffde454e
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-10 18:42:16 +0500

    solve: Mumbling (kyu7)

--- src/main/java/com/hasl/kyu7/Mumbling.java
+++ src/main/java/com/hasl/kyu7/Mumbling.java
@@ -2,7 +2,28 @@ package com.hasl.kyu7;
 
 public class Mumbling {
     public static String accum(String s) {
-        // TODO: "abcd" → "A-Bb-Ccc-Dddd"
-        return "";
+        s = s.toLowerCase();
+        char[] simbols = s.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < simbols.length; i++) {
+            char c = simbols[i];
+
+            // 1. Заглавная буква
+            result.append(Character.toUpperCase(c));
+
+            // 2. Маленькие буквы (повторяем c i раз)
+            for (int j = 0; j < i; j++) {
+                result.append(c);
+            }
+
+            // 3. Дефис между блоками
+            if (i < simbols.length - 1) {
+                result.append("-");
+            }
+        }
+        return result.toString();
     }
 }
+
+// TODO: "abcd" → "A-Bb-Ccc-Dddd"
