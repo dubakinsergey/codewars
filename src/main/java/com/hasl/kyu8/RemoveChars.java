26ea139473d780523610cd76a418372f31e62fab
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-11 10:32:27 +0500

    solve: RemoveChars (kyu8)

--- src/main/java/com/hasl/kyu8/RemoveChars.java
+++ src/main/java/com/hasl/kyu8/RemoveChars.java
@@ -2,8 +2,12 @@ package com.hasl.kyu8;
 
 public class RemoveChars {
     public static String remove(String str) {
-        // "eloquent" → "loquen"
-        // "country" → "ountr"
-        return "";
+        if (str == null || str.length() <= 2) {
+            return "";
+        } else
+            return str.substring(1, str.length() - 1);
     }
 }
+
+// "eloquent" → "loquen"
+// "country" → "ountr"
