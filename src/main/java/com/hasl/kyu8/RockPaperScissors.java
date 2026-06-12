d06b75858030b262b6d01add8d668b06fce76581
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-12 17:03:24 +0500

    solve: RockPaperScissors (kyu8)

--- src/main/java/com/hasl/kyu8/RockPaperScissors.java
+++ src/main/java/com/hasl/kyu8/RockPaperScissors.java
@@ -2,10 +2,23 @@ package com.hasl.kyu8;
 
 public class RockPaperScissors {
     public static String rps(String p1, String p2) {
-        // "rock", "scissors" → "Player 1 won!"
-        // "scissors", "paper" → "Player 1 won!"
-        // "paper", "rock" → "Player 1 won!"
-        // иначе "Player 2 won!" или "Draw!"
-        return "";
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+        if (p1.equals("rock") && p2.equals("scissors")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won!";
+        }
+        return "Player 2 won!";
     }
 }
+
+// "rock", "scissors" → Player 1 won!
+// "scissors", "paper" → "Player 1 won!"
+// "paper", "rock" → "Player 1 won!"
+// иначе "Player 2 won!" или "Draw!"
