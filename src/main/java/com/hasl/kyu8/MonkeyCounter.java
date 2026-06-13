3d04163400dd2321ed7b240dfdd9f5b1479e2dd2
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-13 12:05:53 +0500

    solve: MonkeyCounter (kyu8) - last 8kyu task

--- src/main/java/com/hasl/kyu8/MonkeyCounter.java
+++ src/main/java/com/hasl/kyu8/MonkeyCounter.java
@@ -2,7 +2,13 @@ package com.hasl.kyu8;
 
 public class MonkeyCounter {
     public static int[] monkeyCount(int n) {
-        // 5 → [1, 2, 3, 4, 5]
-        return new int[]{};
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+        return arr;
     }
 }
+
+// 5 → [1, 2, 3, 4, 5]
