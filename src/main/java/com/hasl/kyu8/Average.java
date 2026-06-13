ef77ba3dcd156b69c39930dd0866b95bc2a28b61
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-13 11:49:36 +0500

    solve: Average (kyu8)

--- src/main/java/com/hasl/kyu8/Average.java
+++ src/main/java/com/hasl/kyu8/Average.java
@@ -2,8 +2,18 @@ package com.hasl.kyu8;
 
 public class Average {
     public static double findAverage(int[] array) {
-        // [1, 2, 3] → 2.0
-        // [] → 0.0
-        return 0;
+        double sum = 0;
+
+        if (array == null || array.length == 0) {
+            return 0.0;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / array.length;
     }
 }
+
+// [1, 2, 3] → 2.0
+// [] → 0.0
