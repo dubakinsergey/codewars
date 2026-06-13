72d0d63917834613631cd19aff5ea246a0d08477
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-13 16:15:56 +0500

    getCountByValueOf

--- src/main/java/com/hasl/kyu7/VowelCount.java
+++ src/main/java/com/hasl/kyu7/VowelCount.java
@@ -1,8 +1,18 @@
 package com.hasl.kyu7;
 
 public class VowelCount {
-    public static int getCount(String str) {
-        // "abracadabra" → 5 (a,a,a,a,a)
-        return 0;
+    public static int getCountByValueOf(String str) {
+
+        char[] chars = str.toCharArray();
+        String vowels = "AEIOUaeiou";
+        int count = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (vowels.contains(String.valueOf(chars[i])))
+                count++;
+        }
+        return count;
     }
 }
+
+// "abracadabra" → 5 (a,a,a,a,a)
--- src/test/java/com/hasl/kyu7/VowelCountTest.java
+++ src/test/java/com/hasl/kyu7/VowelCountTest.java
@@ -8,10 +8,10 @@ public class VowelCountTest {
 
     @Test
     public void testGetCount() {
-        assertThat(VowelCount.getCount("abracadabra")).isEqualTo(5);
-        assertThat(VowelCount.getCount("")).isZero();
-        assertThat(VowelCount.getCount("bcdfg")).isZero();
-        assertThat(VowelCount.getCount("aeiou")).isEqualTo(5);
-        assertThat(VowelCount.getCount("AEIOU")).isEqualTo(5);
+        assertThat(VowelCount.getCountByValueOf("abracadabra")).isEqualTo(5);
+        assertThat(VowelCount.getCountByValueOf("")).isZero();
+        assertThat(VowelCount.getCountByValueOf("bcdfg")).isZero();
+        assertThat(VowelCount.getCountByValueOf("aeiou")).isEqualTo(5);
+        assertThat(VowelCount.getCountByValueOf("AEIOU")).isEqualTo(5);
     }
 }
