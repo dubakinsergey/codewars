9af56acdff5c79145451805cc0406e4d00634ea3
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-13 11:02:32 +0500

    solve: SmallestInteger with two implementations (sort + loop)

--- src/main/java/com/hasl/kyu8/SmallestInteger.java
+++ src/main/java/com/hasl/kyu8/SmallestInteger.java
@@ -7,6 +7,18 @@ public class SmallestInteger {
         Arrays.sort(args);
         return args[0];
     }
+
+    public static int findSmallestIntByFor(int[] args) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] < min) {
+                min = args[i];
+            }
+        }
+        return min;
+    }
 }
 
 // [34, 15, 88, 2] → 2
--- src/test/java/com/hasl/kyu8/SmallestIntegerTest.java
+++ src/test/java/com/hasl/kyu8/SmallestIntegerTest.java
@@ -11,5 +11,9 @@ public class SmallestIntegerTest {
         assertThat(SmallestInteger.findSmallestIntBySort(new int[]{34, 15, 88, 2})).isEqualTo(2);
         assertThat(SmallestInteger.findSmallestIntBySort(new int[]{-1, -5, 0, 10})).isEqualTo(-5);
         assertThat(SmallestInteger.findSmallestIntBySort(new int[]{100})).isEqualTo(100);
+        System.out.println("-------------------");
+        assertThat(SmallestInteger.findSmallestIntByFor(new int[]{34, 15, 88, 2})).isEqualTo(2);
+        assertThat(SmallestInteger.findSmallestIntByFor(new int[]{-1, -5, 0, 10})).isEqualTo(-5);
+        assertThat(SmallestInteger.findSmallestIntByFor(new int[]{100})).isEqualTo(100);
     }
 }
