8b225a69c1d8aa755191f3df7194d519330a3389
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-13 16:58:55 +0500

    ExesOhs

--- src/main/java/com/hasl/kyu7/ExesOhs.java
+++ src/main/java/com/hasl/kyu7/ExesOhs.java
@@ -2,9 +2,23 @@ package com.hasl.kyu7;
 
 public class ExesOhs {
     public static boolean getXO(String str) {
-        // "xo" → true
-        // "xxOo" → true
-        // "xxxm" → false
-        return false;
+
+        String lower = str.toLowerCase();
+        int xCount = 0;
+        int oCount = 0;
+
+        for (int i = 0; i < lower.length(); i++) {
+            if (lower.charAt(i) == 'x') {
+                xCount++;
+            }
+            if (lower.charAt(i) == 'o') {
+                oCount++;
+            }
+        }
+        return xCount == oCount;
     }
 }
+
+// "xo" → true
+// "xxOo" → true
+// "xxxm" → false
