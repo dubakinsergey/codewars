d092b0c2c39d3a12beaf901e7769492524217dd2
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-14 15:44:01 +0500

    DnaStrand

--- src/main/java/com/hasl/kyu7/DnaStrand.java
+++ src/main/java/com/hasl/kyu7/DnaStrand.java
@@ -2,8 +2,22 @@ package com.hasl.kyu7;
 
 public class DnaStrand {
     public static String makeComplement(String dna) {
-        // "ATTGC" → "TAACG"
-        // "GTAT" → "CATA"
-        return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char el : dna.toCharArray()) {
+
+            switch (el) {
+                case 'A' -> sb.append('T');
+                case 'T' -> sb.append('A');
+                case 'C' -> sb.append('G');
+                case 'G' -> sb.append('C');
+                default -> sb.append(el); // на случай других символов
+            }
+        }
+        return sb.toString();
     }
 }
+
+// "ATTGC" → "TAACG"
+// "GTAT" → "CATA"
