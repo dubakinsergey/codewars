6ff9dd781b606dcc98a23b8d35fa13f793f2bf24
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-14 16:09:37 +0500

    ReverseWordsTwo

--- src/main/java/com/hasl/kyu7/ReverseWordsTwo.java
+++ src/main/java/com/hasl/kyu7/ReverseWordsTwo.java
@@ -2,9 +2,21 @@ package com.hasl.kyu7;
 
 public class ReverseWordsTwo {
     public static String reverseWordsTwo(String original) {
-        // "The quick brown fox jumps over the lazy dog."
-        // → "ehT kciuq nworb xof spmuj revo eht yzal .god"
-        // пробелы сохранить
-        return "";
+        String[] words = original.split(" ", -1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String reversedWord = new StringBuilder(words[i]).reverse().toString();
+            sb.append(reversedWord);
+
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
 }
+
+// "The quick brown fox jumps over the lazy dog."
+// → "ehT kciuq nworb xof spmuj revo eht yzal .god"
+// пробелы сохранить
