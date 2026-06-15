7064cae64227b453caf451c06cfc48cff3ccf037
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-15 11:37:51 +0500

    solve: Isograms (kyu7) with HashSet

--- src/main/java/com/hasl/kyu7/Isograms.java
+++ src/main/java/com/hasl/kyu7/Isograms.java
@@ -1,10 +1,20 @@
 package com.hasl.kyu7;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Isograms {
     public static boolean isIsogram(String str) {
-        // "Dermatoglyphics" → true
-        // "aba" → false
-        // "moOse" → false (регистр не важен)
-        return false;
+        String lower = str.toLowerCase();
+        Set<Character> set = new HashSet<>();
+
+        for (char el : lower.toCharArray()) {
+            set.add(el);
+        }
+        return set.size() == lower.length();
     }
 }
+
+// "Dermatoglyphics" → true
+// "aba" → false
+// "moOse" → false (регистр не важен)
