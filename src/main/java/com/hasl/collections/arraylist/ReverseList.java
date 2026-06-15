67dfcf741bd506aa0bd02634193063c993cd93b8
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-15 19:47:11 +0500

    solve: ReverseList (ArrayList)

--- src/main/java/com/hasl/collections/arraylist/ReverseList.java
+++ src/main/java/com/hasl/collections/arraylist/ReverseList.java
@@ -5,8 +5,18 @@ import java.util.List;
 
 public class ReverseList {
     public static List<Integer> reverse(List<Integer> list) {
-        // [1, 2, 3, 4] → [4, 3, 2, 1]
-        // запрещено использовать Collections.reverse()
-        return new ArrayList<>();
+
+        if (list.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<Integer> reversedList = new ArrayList<>();
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reversedList.add(list.get(i));
+        }
+        return reversedList;
     }
 }
+
+// [1, 2, 3, 4] → [4, 3, 2, 1]
+// запрещено использовать Collections.reverse()
