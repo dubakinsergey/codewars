b34059a01af89b2fcc4d5cff07c6034e09df4496
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-15 11:20:06 +0500

    OpenOrSenior

--- src/main/java/com/hasl/kyu7/OpenOrSenior.java
+++ src/main/java/com/hasl/kyu7/OpenOrSenior.java
@@ -2,8 +2,21 @@ package com.hasl.kyu7;
 
 public class OpenOrSenior {
     public static String[] openOrSenior(int[][] data) {
-        // [[45, 12], [55,21], [19, -2], [104, 20]] → ["Open", "Senior", "Open", "Senior"]
-        // Senior: возраст >= 55 и гандикап > 7
-        return new String[]{};
+        String[] result = new String[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            // Получи возраст и гандикап
+            int age = data[i][0];
+            int handicap = data[i][1];
+
+            // Проверь условие Senior
+            if (age >= 55 && handicap > 7) {
+                result[i] = "Senior";
+            } else result[i] = "Open";
+        }
+        return result;
     }
 }
+
+// [[45, 12], [55,21], [19, -2], [104, 20]] → ["Open", "Senior", "Open", "Senior"]
+// Senior: возраст >= 55 и гандикап > 7
