1049475305a4b4c1ef0af4721374a7d15e28112e
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-16 11:20:36 +0500

    solve: ArrayDiff (ArrayList) - last ArrayList task

--- src/main/java/com/hasl/collections/arraylist/ArrayDiff.java
+++ src/main/java/com/hasl/collections/arraylist/ArrayDiff.java
@@ -5,8 +5,18 @@ import java.util.List;
 
 public class ArrayDiff {
     public static List<Integer> arrayDiff(List<Integer> a, List<Integer> b) {
-        // [1, 2, 2, 3], [2] → [1, 3]
-        // удалить из a все элементы, которые есть в b
-        return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            Integer num = a.get(i);
+
+            if (!b.contains(num)) {
+                result.add(num);
+            }
+        }
+        return result;
     }
 }
+
+// [1, 2, 2, 3], [2] → [1, 3]
+// удалить из a все элементы, которые есть в b
