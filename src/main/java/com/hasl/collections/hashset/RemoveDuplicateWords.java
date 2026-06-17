f14c998ba4a52e2cd179971582bcaae6e3b81d2b
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-17 14:05:35 +0500

    RemoveDuplicateWords

--- src/main/java/com/hasl/collections/hashset/RemoveDuplicateWords.java
+++ src/main/java/com/hasl/collections/hashset/RemoveDuplicateWords.java
@@ -1,9 +1,20 @@
 package com.hasl.collections.hashset;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class RemoveDuplicateWords {
     public static String removeDuplicates(String sentence) {
-        // "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
-        // → "alpha beta gamma delta"
-        return "";
+
+        String[] word = sentence.split(" ");
+        Set<String> set = new LinkedHashSet<>(); // вместо HashSet (что бы сохранить порядок)
+
+        for (int i = 0; i < word.length; i++) {
+            set.add(word[i]);
+        }
+        return String.join(" ", set);
     }
 }
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+// → "alpha beta gamma delta"
