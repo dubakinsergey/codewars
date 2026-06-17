69fb9edb54b2dd76cc732b2a5c74ab2e786fa33a
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-17 16:14:35 +0500

    CharacterFrequency

--- src/main/java/com/hasl/collections/hashmap/CharacterFrequency.java
+++ src/main/java/com/hasl/collections/hashmap/CharacterFrequency.java
@@ -5,7 +5,14 @@ import java.util.Map;
 
 public class CharacterFrequency {
     public static Map<Character, Integer> count(String str) {
-        // "banana" → {b=1, a=3, n=2}
-        return new HashMap<>();
+
+        Map<Character, Integer> mapResult = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            mapResult.put(c, mapResult.getOrDefault(c, 0) + 1);
+        }
+        return mapResult;
     }
 }
+
+// "banana" → {b=1, a=3, n=2}
