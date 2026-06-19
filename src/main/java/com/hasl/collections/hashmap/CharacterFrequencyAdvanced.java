6d976e9c6a2488c60b97101b64f6df5604753192
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-19 10:16:08 +0500

    solve: CharacterFrequencyAdvanced (HashMap)

--- src/main/java/com/hasl/collections/hashmap/CharacterFrequencyAdvanced.java
+++ src/main/java/com/hasl/collections/hashmap/CharacterFrequencyAdvanced.java
@@ -1,10 +1,25 @@
 package com.hasl.collections.hashmap;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CharacterFrequencyAdvanced {
     public static Map<Character, Integer> count(String str) {
-        // "Hello World" → {h=1, e=1, l=3, o=2, w=1, r=1, d=1}
-        return Map.of();
+
+        if (str == null || str.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        char[] charsLower = str.toLowerCase().toCharArray();
+        Map<Character, Integer> mapResult = new HashMap<>();
+
+        for (char el : charsLower) {
+            if (el != ' ')
+                mapResult.put(el, mapResult.getOrDefault(el, 0) + 1);
+        }
+
+        return mapResult;
     }
 }
+
+// "Hello World" → {h=1, e=1, l=3, o=2, w=1, r=1, d=1}
