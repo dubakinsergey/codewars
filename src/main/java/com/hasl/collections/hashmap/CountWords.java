aef0bfed08c7b874507442272bdbea5b170c6fce
Sergey Dubakin <dubakinsergey@gmail.com>
2026-06-19 10:01:39 +0500

    CountWords

--- src/main/java/com/hasl/collections/hashmap/CountWords.java
+++ src/main/java/com/hasl/collections/hashmap/CountWords.java
@@ -1,10 +1,24 @@
 package com.hasl.collections.hashmap;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CountWords {
     public static Map<String, Integer> count(String sentence) {
-        // "one two three one two one" → {one=3, two=2, three=1}
-        return Map.of();
+
+        if (sentence == null || sentence.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        String[] words = sentence.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String el : words) {
+            map.put(el, map.getOrDefault(el, 0) + 1);
+        }
+        return map;
     }
 }
+
+// "one two three one two one" → {one=3, two=2, three=1}
