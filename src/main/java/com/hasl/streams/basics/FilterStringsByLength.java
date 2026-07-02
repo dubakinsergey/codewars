32b7e473ba2617e3af298e38f9095a56f0a3e34f
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-02 21:31:23 +0500

    solve: FilterStringsByLength (Stream API)

--- src/main/java/com/hasl/streams/basics/FilterStringsByLength.java
+++ src/main/java/com/hasl/streams/basics/FilterStringsByLength.java
@@ -1,10 +1,13 @@
 package com.hasl.streams.basics;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterStringsByLength {
     public static List<String> filterByLength(List<String> strings, int minLength) {
-        // ["a", "ab", "abc", "abcd"], 3 → ["abc", "abcd"]
-        return List.of();
+        return strings.stream()
+                .filter(el -> el.length() >= minLength)
+                .collect(Collectors.toList());
     }
 }
+// ["a", "ab", "abc", "abcd"], 3 → ["abc", "abcd"]
