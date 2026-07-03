d91e5f658da676c7ab7c1e87e577c654a9f8175b
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-03 09:40:58 +0500

    solve: CollectToList (Stream API)

--- src/main/java/com/hasl/streams/basics/CollectToList.java
+++ src/main/java/com/hasl/streams/basics/CollectToList.java
@@ -1,10 +1,13 @@
 package com.hasl.streams.basics;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectToList {
     public static List<Integer> collectSquares(List<Integer> numbers) {
-        // [1, 2, 3] → [1, 4, 9] (использовать map + collect)
-        return List.of();
+        return numbers.stream()
+                .map(el -> el * el)
+                .collect(Collectors.toList());
     }
 }
+// [1, 2, 3] → [1, 4, 9] (использовать map + collect)
