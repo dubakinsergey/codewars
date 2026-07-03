c9511e99f485cd697d91260057ce17a2af3c29ec
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-03 09:46:29 +0500

    solve: FilterAndMap (Stream API) - last Block 1 task

--- src/main/java/com/hasl/streams/basics/FilterAndMap.java
+++ src/main/java/com/hasl/streams/basics/FilterAndMap.java
@@ -1,10 +1,14 @@
 package com.hasl.streams.basics;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterAndMap {
     public static List<Integer> filterAndSquare(List<Integer> numbers) {
-        // [1, 2, 3, 6, 7, 8] → отфильтровать > 5 → [6, 7, 8] → возвести в квадрат → [36, 49, 64]
-        return List.of();
+        return numbers.stream()
+                .filter(el -> el > 5)
+                .map(el -> el * el)
+                .collect(Collectors.toList());
     }
 }
+// [1, 2, 3, 6, 7, 8] → отфильтровать > 5 → [6, 7, 8] → возвести в квадрат → [36, 49, 64]
