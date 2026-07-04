8e4f9f79dfc835b62273f1815540e7eb49ca1cde
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-04 10:25:35 +0500

    solve: GroupByLength (Stream API)

--- src/main/java/com/hasl/streams/grouping/GroupByLength.java
+++ src/main/java/com/hasl/streams/grouping/GroupByLength.java
@@ -2,10 +2,12 @@ package com.hasl.streams.grouping;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupByLength {
     public static Map<Integer, List<String>> groupByLength(List<String> words) {
-        // ["a", "ab", "abc", "ab", "abcd"] → {1=[a], 2=[ab, ab], 3=[abc], 4=[abcd]}
-        return Map.of();
+        return words.stream()
+                .collect(Collectors.groupingBy(String::length));
     }
 }
+// ["a", "ab", "abc", "ab", "abcd"] → {1=[a], 2=[ab, ab], 3=[abc], 4=[abcd]}
