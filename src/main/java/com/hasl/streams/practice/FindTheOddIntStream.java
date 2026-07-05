9b376310bd14ec072cb7d4454d3fbc03235a7473
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-05 17:27:15 +0500

    FindTheOddIntStream

--- src/main/java/com/hasl/streams/practice/FindTheOddIntStream.java
+++ src/main/java/com/hasl/streams/practice/FindTheOddIntStream.java
@@ -1,10 +1,21 @@
 package com.hasl.streams.practice;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FindTheOddIntStream {
     public static int findOdd(List<Integer> numbers) {
-        // [1, 2, 2, 3, 3, 3, 4, 4, 4, 4] → 3
-        return 0;
+        return numbers.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() % 2 == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(0);
     }
 }
+
+// [1, 2, 2, 3, 3, 3, 4, 4, 4, 4] → 3
