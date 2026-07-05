06e4f2b6ca7701302442678d0a1c6ad18126d0e4
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-05 16:54:07 +0500

    solve: ArrayDiffStream (Stream API)

--- src/main/java/com/hasl/streams/practice/ArrayDiffStream.java
+++ src/main/java/com/hasl/streams/practice/ArrayDiffStream.java
@@ -1,10 +1,14 @@
 package com.hasl.streams.practice;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayDiffStream {
     public static List<Integer> arrayDiff(List<Integer> a, List<Integer> b) {
-        // [1, 2, 2, 3], [2] → [1, 3]
-        return List.of();
+        return a.stream()
+                .filter(el -> !b.contains(el))
+                .collect(Collectors.toList());
     }
 }
+
+// [1, 2, 2, 3], [2] → [1, 3]
