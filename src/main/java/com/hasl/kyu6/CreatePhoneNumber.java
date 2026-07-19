99bb61de4b12840fd8095028d6f2a9bae93ff1eb
Sergey Dubakin <dubakinsergey@gmail.com>
2026-07-19 17:21:01 +0500

    CreatePhoneNumber

--- src/main/java/com/hasl/kyu6/CreatePhoneNumber.java
+++ src/main/java/com/hasl/kyu6/CreatePhoneNumber.java
@@ -2,7 +2,9 @@ package com.hasl.kyu6;
 
 public class CreatePhoneNumber {
     public static String createPhoneNumber(int[] numbers) {
-        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] → "(123) 456-7890"
-        return "";
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", numbers[0], numbers[1], numbers[2], numbers[3],
+                numbers[4], numbers[5], numbers[6], numbers[7], numbers[8], numbers[9]);
     }
 }
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] → "(123) 456-7890"
